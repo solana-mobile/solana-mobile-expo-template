a32f9d030c425c8eda065f1b54e0378c71ace18e
michael sulistio <michaelsulistio@gmail.com>
2024-02-29 13:12:52 -0800

    [v2] TopBar connect button

--- src/components/top-bar/top-bar-feature.tsx
+++ src/components/top-bar/top-bar-feature.tsx
@@ -1,13 +1,18 @@
 import { Pressable, Text } from "react-native";
 import { Appbar } from "react-native-paper";
-import { WalletButton } from "./top-bar-ui";
+import { TopBarWalletButton } from "./top-bar-ui";
 
 export function TopBar() {
   return (
-    <Appbar.Header mode="small">
+    <Appbar.Header mode="medium">
       <Appbar.Content title="Solana Expo Template" />
-      <WalletButton />
-      <Appbar.Action icon="cog-outline" onPress={() => {}} />
+      {/* <Appbar.Action icon="wallet" onPress={() => {}} /> */}
+      <Appbar.Action
+        icon="cog-outline"
+        mode="contained-tonal"
+        onPress={() => {}}
+      />
+      <TopBarWalletButton />
     </Appbar.Header>
   );
 }
--- src/components/top-bar/top-bar-ui.tsx
+++ src/components/top-bar/top-bar-ui.tsx
@@ -1,6 +1,7 @@
 import { Button } from "react-native-paper";
 import { useAuthorization } from "../../utils/useAuthorization";
 import { useMobileWallet } from "../../utils/useMobileWallet";
+import { StyleSheet } from "react-native";
 
 function truncateAddress(address: string): string {
   return `${address.slice(0, 3)}...${address.slice(
@@ -9,13 +10,14 @@ function truncateAddress(address: string): string {
   )}`;
 }
 
-export function WalletButton() {
+export function TopBarWalletButton() {
   const { selectedAccount } = useAuthorization();
   const { connect, disconnect } = useMobileWallet();
   return (
     <Button
       icon="wallet"
-      mode="contained"
+      mode="contained-tonal"
+      style={styles.button}
       onPress={selectedAccount ? disconnect : connect}
     >
       {selectedAccount
@@ -24,3 +26,9 @@ export function WalletButton() {
     </Button>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: "center",
+  },
+});
